91d8d9e6cac3a2d9bc13e8a123c98970a79f2612
Arthur Ribeiro <arthurar600@gmail.com>
2026-09-19 20:20:02 -0300

    Add files via upload

--- Atividade 1/main.cpp
+++ Atividade 1/main.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
+#include <cstdlib>
 #include "aluno.h"
 
 int main(void)
 {
-    aluno y[2];
+    aluno* y;
+    int i{0}, tam{2};
+    char escolha{'S'};
+    y = (aluno*)malloc(tam * sizeof(aluno));
 
-    ler_aluno(y[0]);
-    ficha(y[0]);
+    do
+    {
+        ler_aluno(y[i]);
+        std::cout << "Deseja continuar registrando alunos? (S/N)\n";
+        std::cin >> escolha >> std::ws;
 
+        if(i == tam)
+        {
+            std::cout << "O |if| foi acionado\n";
+            tam += 2;
+            y = (aluno*)realloc(y, tam * sizeof(aluno));
+        }
+        i++;
+    }while(escolha == 'S' || escolha == 's');
+    
+free(y);
 return 0;
 }
