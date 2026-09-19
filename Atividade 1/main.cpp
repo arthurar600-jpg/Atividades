8d07fa96d7522aaadfda916613f583abe55ee31e
Arthur Ribeiro <arthurar600@gmail.com>
2026-09-19 20:34:02 -0300

    Add files via upload
    Tentativa de concertar o buffer de teclado, após pesquisa na internet sobre o assunto, aparentemente bem sucedida.
    Porém existe algum erro na realocação de memoria, visto que o programa fecha assim que ela deveria acontecer.
    Tentar concertar isso após mais estudo sobre C++.

--- Atividade 1/main.cpp
+++ Atividade 1/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <limits> // estudar isso
 #include "aluno.h"
 
 int main(void)
@@ -11,16 +12,18 @@ int main(void)
 
     do
     {
-        ler_aluno(y[i]);
-        std::cout << "Deseja continuar registrando alunos? (S/N)\n";
-        std::cin >> escolha >> std::ws;
-
-        if(i == tam)
+         if(i == tam)
         {
             std::cout << "O |if| foi acionado\n";
             tam += 2;
             y = (aluno*)realloc(y, tam * sizeof(aluno));
         }
+
+        ler_aluno(y[i]);
+        std::cout << "Deseja continuar registrando alunos? (S/N)\n";
+        std::cin >> escolha;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // estudar isso
+
         i++;
     }while(escolha == 'S' || escolha == 's');
     
